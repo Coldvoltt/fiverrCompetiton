885e0147a63a9d18a0b35c0461241d79f0463726
Paul Alexander <otulajapaul@outlook.com>
2022-09-13 14:32:30 +0100

    catboost fixed

--- catBoost.R
+++ catBoost.R
@@ -20,11 +20,11 @@ ts <- df[-samp,]
 
 #Creating a recipe with pre-processing
 trRecipe <- recipe(label~., data = tr) |>  
-  step_impute_median(all_numeric_predictors()) |> 
+  step_impute_median(all_numeric_predictors()) |> # Median Imputation
   step_zv(all_predictors()) |>   # Removes variables containing single values
-  step_nzv(all_predictors()) |>   #%>% # Removes variables that are sparse and unbalanced
+  step_nzv(all_predictors()) |>  # Removes variables that are sparse and unbalanced
   step_normalize(all_numeric_predictors()) |>   # Transforms data to have mean 0 and SD 1
-  step_smote(label)
+  step_smote(label)   # Tackle class imbalance
 
 # juicing pre-processed dataframe
 pTrain<- trRecipe |> 
@@ -35,35 +35,34 @@ pTest<- trRecipe |>
   prep() |> 
   bake(ts)
 
+trY=as.numeric(levels(pTrain$label))[pTrain$label]
+tsY=as.numeric(levels(pTest$label))[pTest$label]
 
+train_pool<- catboost.load_pool(pTrain[,-33], label =trY)
+test_pool<- catboost.load_pool(pTest[,-33], label = tsY)
 
-trY<- as.numeric(pTrain$label)
-tsY<- as.numeric(pTest$label)
-
-train<- catboost.load_pool(data = pTrain[,-1], label = trY)
-test<- catboost.load_pool(data = pTest[,-1], label = tsY)
-
-params<- list(iterations = 1500,
+fit_params<- list(iterations = 105,
+              random_seed = 50,
+              loss_function = 'Logloss',
               use_best_model = TRUE)
 
-catBoostMod<- catboost.train(train,test, params)
+catBoostMod<- catboost.train(learn_pool = train_pool, test = test_pool, params = fit_params)
 
 
 
-val<- testdf |> 
-  select(-user_id)
+val<- testdf
 
-# vald<-trRecipe |> 
-#   prep() |> 
-#   bake(val) 
+vald<-trRecipe |>
+  prep() |>
+  bake(val)
 
-vald<- val|> 
+vald_pool<- vald|> 
   catboost.load_pool()
 
 
-predDf<- catboost.predict(catBoostMod,vald)
-predDf<- as.numeric(predDf>.5)
+predDf<- catboost.predict(catBoostMod,vald_pool, prediction_type = "Class")
+
 submission2<- as.data.frame(cbind(user_id = testdf$user_id, prediction = predDf))
 
 
-write.csv(submission2, "paulie2.csv")
+write.csv(submission2, "catBoost.csv")
