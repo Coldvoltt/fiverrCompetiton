b211799d7c1aaf594e7d79b15b099143991f126f
Paul Alexander <otulajapaul@outlook.com>
2022-09-14 22:28:50 +0100

    XGboost edit

--- script2.R
+++ script2.R
@@ -1,112 +1,55 @@
 library(tidyverse)
-library(tidymodels)
+library(catboost)
 library(caret)
+library(tidymodels)
 library(themis)
+library(tidy.outliers)
 
 read_csv('train.csv') -> df
-read_csv('test.csv') -> test
+read_csv('test.csv') -> testdf
 
-sum(is.na(df)) # Check for NA
-
-df <- df |> 
+df <- df |>
   mutate(label = as.factor(label)) |> 
   select(-user_id)
 
-# The next line of code asks R to check if 
-# each vector or column in DF is <= 6. we store 
-# The result of the check is stored in abc.
-
-# abc <-sapply(df, n_distinct) <= 6
-# abd <- sapply(test, n_distinct) <= 6
-
-# We then ask R to convert every column which
-# has distinct values <= 6 as factor.
-
-# df<- df |> 
-#   mutate_if(abc,as.factor) |> 
-#   mutate(X13 = as.factor(X13),
-#          X40 = as.factor(X40))
-
-# test<- test |> 
-#   mutate_if(abd, as.factor)
-
-
-df |> 
-  count(label)
-
-prop.table(table(df$label))
-
-# Partitioning data for local training and testing
-set.seed(45)
-samp <- createDataPartition(df$label, p = .85, list = FALSE)
-tr <- df[samp,] 
-ts <- df[-samp,]
-
-
-#-----------------------------------
 
 #Creating a recipe with pre-processing
-trRecipe <- recipe(label~., data = tr) |>  
-  step_impute_median(all_numeric_predictors()) |> 
+trRecipe <- recipe(label~., data = df) |>  
+  step_impute_median(all_numeric_predictors()) |> # Median Imputation
   step_zv(all_predictors()) |>   # Removes variables containing single values
-  step_nzv(all_predictors()) |>   #%>% # Removes variables that are sparse and unbalanced
+  step_nzv(all_predictors()) |>  # Removes variables that are sparse and unbalanced
   step_normalize(all_numeric_predictors()) |>   # Transforms data to have mean 0 and SD 1
-  step_smote(label)
+  step_smote(label)   # Tackle class imbalance
 
 # juicing pre-processed dataframe
 pTrain<- trRecipe |> 
   prep() |> 
-  juice()
+  juice() |> 
+  as.data.frame()
 
-pTest<- trRecipe |> 
-  prep() |> 
-  bake(ts)
-
-# Creating Data Matrix
-trMatrix<- data.matrix(pTrain[,-33])
-trY<- as.numeric(pTrain$label)-1
-
-tsMatrix<- data.matrix(pTest[,-33])
-tsY<- as.numeric(pTest$label)-1
-
-dTrain<- xgb.DMatrix(data = trMatrix, label = trY)
-dTest<- xgb.DMatrix(data = tsMatrix, label = tsY)
-
-#-----------------------------------
 library(xgboost)
-set.seed(123)
 
-# booster = 'gbtree': Possible to also have linear boosters as your weak learners.
-params_booster <- list(booster = 'gbtree',
-                       eta = 1, gamma = 0,
-                       max.depth = 2,
-                       subsample = 1,
-                       colsample_bytree = 1,
-                       min_child_weight = 1,
-                       objective = "binary:logistic")
+y = as.numeric(levels(pTrain$label))[pTrain$label]
 
-bst.cv <- xgb.cv(data = trMatrix, 
-                 label = trY,
-                 params = params_booster,
-                 nrounds = 300, 
-                 nfold = 5,
-                 print_every_n = 20,
-                 verbose = 2)
+xgbFinal<- xgboost(data=data.matrix(pTrain[,-33]),
+             label= y,
+             nround = 5000,
+             objective = "binary:logistic",
+             eval_metric = "logloss")
+
+val<- testdf
+
+valMat<-trRecipe |>
+  prep() |>
+  bake(val) |> 
+  data.matrix()
 
 
-bstSparse <- xgboost(data = trMatrix, label = trY, nrounds = 320, params = params_booster)
+pred<- predict(xgbFinal, newdata = valMat)
+pred<- ifelse(pred>.5,1,0)
 
 
-test1<- trRecipe |> 
-  prep() |> 
-  bake(test)
-
-testPredictDf<- data.matrix(test1)
+submission4<- as.data.frame(cbind(user_id = testdf$user_id, prediction = predDf))
 
 
-pred<- predict(bstSparse, testPredictDf)
-pred<- as.numeric(pred>.5)
-submission<- as.data.frame(cbind(user_id = test$user_id, prediction = pred))
-
-
-write.csv(submission, "paulie.csv")
+write.csv(submission4, "xgboost4.csv")
